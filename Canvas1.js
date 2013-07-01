7b7bfc212044021f18c5b2295893cc1ad3d935a4
J.A. Simmons V <simmonsj@jasimmonsv.com>
2013-07-01 15:40:39 -0500

    Canvas Quiz 3

--- Canvas1.js
+++ Canvas1.js
@@ -1,30 +1,34 @@
 
 
+var canvas = null;
+var context = null;
+var img = null;
 
-setup = function() {
-	var body = document.getElementById('body');
-	var canvas = document.createElement('canvas');
+var onImageLoad = function(){
+	console.log("IMAGE!!!");
+	// Draw an image to the canvas at position 192,192.
+	// Remember that the drawImage method is attached
+	// to the 2D Context, not the canvas element!
+	// YOUR CODE HERE
+    context.drawImage(this.img,192,192);
+};
 
-	var context = canvas.getContext('2d');
-	
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+var setup = function() {
+	var body = document.getElementById("body");
+	canvas = document.createElement("canvas");
+
+	context = canvas.getContext('2d');
+
+	canvas.setAttribute('width', 500);
+	canvas.setAttribute('height', 700);
 
 	body.appendChild(canvas);
 
-	// Create a new image with a src of "/media/js/standalone/libs/gamedev_assets/ralphyrobot.png" and onload of onImageLoad
-	// YOUR CODE HERE
-    var img = new Image();
-    img.onload=onImageLoad;
-    img.src="/media/js/standalone/libs/gamedev_assets/ralphyrobot.png";
+	img = new Image();
+	img.onload = onImageLoad;
+	img.src = "/media/js/standalone/libs/gamedev_assets/ralphyrobot.png";
 };
 
-onImageLoad = function(){
-	// Use the console.log function to print a statement to the browser console.
-	// This will print once the image has been downloaded.
-	// YOUR CODE HERE
-    console.log("Bam!!");
-};
 
 setup();
 
